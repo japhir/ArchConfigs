60029c047416f42d6f1e8c458d08bd6e3b3abdae
Ilja Kocken <iljakocken@gmail.com>
2015-10-02 11:01:26 +0200

    add a ton of new laptop configs and scripts

---
+++ soundwithvolnoti.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# from https://bbs.archlinux.org/viewtopic.php?id=132218
+
+VOLUME=$(pamixer --get-volume)
+VALUE=5
+
+case "$1" in
+    "up")
+	[[ "$VOLUME" -eq 100 ]] && VALUE=0
+	pamixer --increase $VALUE
+	;;
+    "down")
+	pamixer --decrease $VALUE
+	;;
+    "mute")
+	pamixer --toggle-mute
+	;;
+esac
+
+# notification
+VOLUME=$(pamixer --get-volume)
+MUTE=$(pamixer --get-mute)
+
+if [ "$MUTE" == "false" ]; then
+    volnoti-show $VOLUME
+else
+    volnoti-show -m $VOLUME
+fi
