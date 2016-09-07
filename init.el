5eb2f2f4a863fb12d1d3a18ad9cce03fad367e0c
Ilja Kocken <iljakocken@gmail.com>
2016-09-07 19:58:59 +0200

    add init.el

---
+++ init.el
@@ -0,0 +1,27 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives 
+	     '("melpa" . "https://melpa.org/packages/")
+	     '("org"   . "http://orgmode.org/elpa/"))
+(package-initialize)
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(org-agenda-files
+   (quote
+    ("~/Dropbox/MinorRP/LOSCAR/report/report.org" "~/Dropbox/Apps/orgzly/inbox.org" "~/Dropbox/Apps/orgzly/todo.org" "~/Dropbox/Apps/orgzly/calendars/IljaKocken.org" "~/Dropbox/Apps/orgzly/calendars/marinesciences.org" "~/Dropbox/Apps/orgzly/calendars/assistant.org" "~/Dropbox/Apps/orgzly/calendars/ubv.org" "~/Dropbox/Apps/orgzly/calendars/options.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0)))))
