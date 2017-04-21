e99919d481006fab8472317335ade5d4926956d4
Ilja Kocken <iljakocken@gmail.com>
2017-04-21 17:47:33 +0200

    try to set up notmuch

--- Laptop/init.el
+++ Laptop/init.el
@@ -16,14 +16,24 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(notmuch-saved-searches
+   (quote
+    ((:name "inbox" :query "tag:inbox" :key "i")
+     (:name "unread" :query "tag:unread" :key "u")
+     (:name "flagged" :query "tag:flagged" :key "f")
+     (:name "sent" :query "tag:sent" :key "t")
+     (:name "drafts" :query "tag:draft" :key "d")
+     (:name "all mail" :query "*" :key "a")
+     (:name "next" :query "tag:next")
+     (:name "waiting" :query "tag:waiting")
+     (:name "survival" :query "tag:survival"))))
  '(org-agenda-files
    (quote
     ("~/Nextcloud/PhD/clumpednotes.org" "~/.dropbox-personal/Dropbox/Apps/orgzly/calendars/uu-periods.org" "~/.dropbox-personal/Dropbox/Apps/orgzly/calendars/sports.org" "~/.dropbox-personal/Dropbox/Apps/orgzly/calendars/options.org" "~/.dropbox-personal/Dropbox/Apps/orgzly/calendars/university.org" "~/.dropbox-personal/Dropbox/Apps/orgzly/calendars/gcal.org" "~/.dropbox-personal/Dropbox/Apps/orgzly/calendars/societies.org" "~/.dropbox-personal/Dropbox/Apps/orgzly/lists.org" "~/.dropbox-personal/Dropbox/MRP/Report/Report.org" "~/.dropbox-personal/Dropbox/MinorRP/LOSCAR/report/report.org" "~/.dropbox-personal/Dropbox/Apps/orgzly/inbox.org" "~/.dropbox-personal/Dropbox/Apps/orgzly/todo.org")))
  '(package-selected-packages
    (quote
-    (notmuch monokai-theme monokai smooth-scrolling hl-todo auctex-latexmk latex-math-preview latex-pretty-symbols latex-preview-pane column-marker ag fontawesome latex-extra outline-magic golden-ratio writeroom-mode use-package telephone-line rainbow-mode rainbow-delimiters polymode pandoc-mode org-ref org-plus-contrib org-pdfview org-pandoc org-bullets markdown-mode ivy-bibtex evil-org evil-nerd-commenter evil-magit evil-escape ess dash-functional counsel beacon auto-complete)))
- '(send-mail-function (quote smtpmail-send-it))
- '(smtpmail-smtp-server "smtp.gmail.com")
+    (smtpmail-multi notmuch monokai-theme monokai smooth-scrolling hl-todo auctex-latexmk latex-math-preview latex-pretty-symbols latex-preview-pane column-marker ag fontawesome latex-extra outline-magic golden-ratio writeroom-mode use-package telephone-line rainbow-mode rainbow-delimiters polymode pandoc-mode org-ref org-plus-contrib org-pdfview org-pandoc org-bullets markdown-mode ivy-bibtex evil-org evil-nerd-commenter evil-magit evil-escape ess dash-functional counsel beacon auto-complete)))
+ '(smtpmail-smtp-server "solismail.uu.nl")
  '(smtpmail-smtp-service 587))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
