110f80a971a1a8a9a621432c016c7e573be18e90
Ilja Kocken <i.j.kocken@uu.nl>
2018-11-16 12:39:23 +0100

    update keybinding for org-gcal-fetch and update init.el

--- Laptop/init.el
+++ Laptop/init.el
@@ -16,13 +16,13 @@
     ("9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" default)))
  '(org-agenda-files
    (quote
-    ("~/org/calendars/uu-periods.org" "~/org/calendars/university.org" "~/org/calendars/sports.org" "~/org/calendars/societies.org" "~/org/calendars/outofoffice.org" "~/org/calendars/options.org" "~/org/calendars/meetings.org" "~/org/calendars/gcal.org" "~/org/calendars/fest.org" "~/org/calendars/balance.org" "~/org/calendars/253plus.org" "~/org/calendars/253.org" "~/org/lists.org" "~/org/clumpednotes.org" "~/org/todo.org" "~/org/inbox.org" "~/SurfDrive/PhD/programming/long-term-etf/long-term-etf.org" "/home/japhir/Dropbox/Apps/orgzly/tickler.org")))
+    ("~/SurfDrive/PhD/programming/standardstats/clumpedefficiency.org" "~/org/calendars/uu-periods.org" "~/org/calendars/university.org" "~/org/calendars/sports.org" "~/org/calendars/societies.org" "~/org/calendars/outofoffice.org" "~/org/calendars/options.org" "~/org/calendars/meetings.org" "~/org/calendars/gcal.org" "~/org/calendars/fest.org" "~/org/calendars/balance.org" "~/org/calendars/253plus.org" "~/org/calendars/253.org" "~/org/lists.org" "~/org/clumpednotes.org" "~/org/todo.org" "~/org/inbox.org" "~/SurfDrive/PhD/programming/long-term-etf/long-term-etf.org" "/home/japhir/Dropbox/Apps/orgzly/tickler.org")))
  '(org-entities-user
    (quote
     (("d18O" "\\ce{\\delta^18O}" nil "&delta;<sup>18</sup>O" "δ18O" "δ18O" "δ¹⁸O"))))
  '(package-selected-packages
    (quote
-    (apropospriate-theme apropospriate ess xresources-theme frames-only-mode org-ref-pdf org-books org-pomodoro diff-hl lispy git-gutter writeroom-mode ivy-yasnippet yasnippet evil-numbers evil-indent-plus evil-commentary evil-snipe evil-easymotion webpaste use-package-chords telephone-line systemd stan-mode ranger rainbow-mode rainbow-delimiters polymode ox-tufte org-ref org-plus-contrib org-pdfview org-noter org-gcal org-bullets openwith mu4e-conversation mu4e-alert monokai-theme leuven-theme ivy-bibtex hl-todo gmail-message-mode fontawesome flycheck evil-org evil-nerd-commenter evil-mu4e evil-magit evil-leader evil-escape evil-collection edit-server diminish define-word counsel-projectile bug-hunter beacon auto-complete ace-window)))
+    (poly-markdown apropospriate-theme apropospriate ess xresources-theme frames-only-mode org-ref-pdf org-books org-pomodoro diff-hl lispy git-gutter writeroom-mode ivy-yasnippet yasnippet evil-numbers evil-indent-plus evil-commentary evil-snipe evil-easymotion webpaste use-package-chords telephone-line systemd stan-mode ranger rainbow-mode rainbow-delimiters polymode ox-tufte org-ref org-plus-contrib org-pdfview org-noter org-gcal org-bullets openwith mu4e-conversation mu4e-alert monokai-theme leuven-theme ivy-bibtex hl-todo gmail-message-mode fontawesome flycheck evil-org evil-nerd-commenter evil-mu4e evil-magit evil-leader evil-escape evil-collection edit-server diminish define-word counsel-projectile bug-hunter beacon auto-complete ace-window)))
  '(safe-local-variable-values (quote ((TeX-master . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
