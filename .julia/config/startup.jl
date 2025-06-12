b7102576cc8a7a91af25be3f210d298bb26cd6ef
Ilja J. Kocken <ikocken@hawaii.edu>
2025-06-12 09:06:31 +0200

    (julia) add startup.jl

---
+++ .julia/config/startup.jl
@@ -0,0 +1,26 @@
+try
+    using Revise
+catch e
+    @warn "Error initializing Revise"
+end
+
+if pwd() != "/home/japhir"
+    try
+        using Pkg
+        Pkg.activate(".")
+    catch e
+        @warn "Error activating package"
+    end
+end
+
+if haskey(ENV, "INSIDE_EMACS")
+    ENV["JULIA_EDITOR"] = "emacsclient"
+else
+    # only use OhMyREPL when not inside emacs
+    # I want emacs to handle syntax highlighting and parentheses etc.
+    try
+        using OhMyREPL
+    catch e
+        @warn "Error initializing OhMyREPL"
+    end
+end
